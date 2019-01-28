555e501f8a4b696f603e4b61ab7f2b810f845700
Marc Schwieterman <marc@marcisme.com>
2019-01-28 12:32:07 -0800

    Initial Commit

---
+++ InfinityAndBeyond/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfinityAndBeyond
+//
+//  Created by Marc Schwieterman on 1/28/19.
+//  Copyright © 2019 Marc Schwieterman Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
