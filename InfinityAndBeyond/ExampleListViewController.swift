ff9fd1df5b38ac55a5bca96f8bec581e6b201ec0
Marc Schwieterman <marc@marcisme.com>
2019-01-29 13:55:53 -0800

    Add Count + 1 example

---
+++ InfinityAndBeyond/ExampleListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ExampleListViewController.swift
+//  InfinityAndBeyond
+//
+//  Created by Marc Schwieterman on 1/29/19.
+//  Copyright © 2019 Marc Schwieterman Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ExampleListViewController: UITableViewController {
+
+  enum Example: Int, CaseIterable {
+    case countPlusOne
+
+    var name: String {
+      switch self {
+      case .countPlusOne:
+        return "Count + 1"
+      }
+    }
+
+    func newViewController() -> UIViewController {
+      let vc: UIViewController
+      switch self {
+      case .countPlusOne:
+        vc = CountPlusOneViewController()
+      }
+      vc.navigationItem.title = name
+      return vc
+    }
+  }
+
+  // MARK: - UITableViewDataSource
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return Example.allCases.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ExampleCell")!
+    let example = Example(rawValue: indexPath.row)!
+    cell.textLabel?.text = example.name
+    return cell
+  }
+
+  // MARK: - UITableViewDelegate
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let example = Example(rawValue: indexPath.row)!
+    navigationController?.pushViewController(example.newViewController(), animated: true)
+  }
+
+}
---
+++ InfinityAndBeyond/Examples/CountPlusOneViewController.swift
@@ -0,0 +1,176 @@
+//
+//  CountPlusOneViewController.swift
+//  InfinityAndBeyond
+//
+//  Created by Marc Schwieterman on 1/28/19.
+//  Copyright © 2019 Marc Schwieterman Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+fileprivate struct Model {
+  let id: Int
+}
+
+fileprivate class NetworkClient {
+  enum Response {
+    case success([Model])
+    case failure(String)
+  }
+
+  private let delay: Double
+  private let failEvery: Int
+  private let callbackQueue: DispatchQueue
+
+  private var requestCount = 0
+
+  init(delay: Double, failEvery: Int, callbackQueue: DispatchQueue) {
+    self.delay = delay
+    self.failEvery = failEvery
+    self.callbackQueue = callbackQueue
+  }
+
+  func fetch(offset: Int, limit: Int, completion: @escaping (Response) -> ()) {
+    requestCount += 1
+    // dispatch to a background queue, as an actual networking implementation would
+    DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + delay) {
+      let models = (offset ..< offset + limit).map { Model(id: $0) }
+      self.callbackQueue.async {
+        if self.requestCount % self.failEvery == 0 {
+          completion(.failure("failing request \(self.requestCount), failing every \(self.failEvery)"))
+        } else {
+          completion(.success(models))
+        }
+      }
+    }
+  }
+}
+
+fileprivate class CountPlusOneCell: UITableViewCell {
+
+  enum State {
+    case loading
+    case loaded(Model)
+    case error
+  }
+
+  static let identifier = "CountPlusOneCell"
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func configure(for state: State) {
+    switch state {
+    case .loading:
+      textLabel?.text = "Loading..."
+      detailTextLabel?.text = nil
+    case .loaded(let model):
+      textLabel?.text = String(model.id)
+      detailTextLabel?.text = nil
+    case .error:
+      textLabel?.text = "Failed to load content"
+      detailTextLabel?.text = "Tap to retry"
+    }
+  }
+}
+
+class CountPlusOneViewController: UITableViewController {
+
+  enum State {
+    case loading
+    case loaded
+    case error
+  }
+
+  private let networkClient = NetworkClient(delay: 1.0, failEvery: 3, callbackQueue: DispatchQueue.main)
+  private let batchSize = 20
+
+  private var models: [Model] = []
+  private var state: State = .loaded
+
+  // MARK: - Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.register(CountPlusOneCell.self, forCellReuseIdentifier: CountPlusOneCell.identifier)
+  }
+
+  // MARK: - UITableViewDataSource
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // +1 for the loading/error state cell
+    return models.count + 1
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: CountPlusOneCell.identifier) as! CountPlusOneCell
+
+    // if we have the model, simply display it
+    if indexPath.row < models.count {
+      let model = models[indexPath.row]
+      cell.configure(for: .loaded(model))
+    } else {
+      // otherwise display the loading state
+      cell.configure(for: .loading)
+      switch state {
+      case .loading:
+        // Ensure we don't make more than one network request at a time.
+        // This can happen if the user scrolls to the bottom of the table, then back up a bit,
+        // then back down before the network request completes.
+        break
+      case .loaded, .error:
+        // If we're in loaded state, then we want to fetch the next batch of data.
+        // If we're in an error state, we'll retry the request. This saves the user
+        // from having to tap to retry, and if they happened to scroll back up then
+        // back down, they may not even notice that an error occurred.
+        fetch()
+      }
+    }
+    return cell
+  }
+
+  // MARK: - UITableViewDelegate
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+
+    // If we're in the error state, set the loading message and retry
+    if state == .error {
+      configureLastRow(for: .loading)
+      fetch()
+    }
+  }
+
+  // MARK: - Implementation
+
+  private func fetch() {
+    // set the loading state, so everything else behaves correctly while the request is pending
+    state = .loading
+    // request the next batch of data
+    networkClient.fetch(offset: models.count, limit: batchSize) { (response) in
+      // update the model/UI and set the state based on the type of response
+      switch response {
+      case .success(let models):
+        self.models.append(contentsOf: models)
+        self.state = .loaded
+        self.tableView.reloadData()
+      case .failure(_):
+        self.state = .error
+        self.configureLastRow(for: .error)
+      }
+    }
+  }
+
+  private func configureLastRow(for state: CountPlusOneCell.State) {
+    let lastIndexPath = IndexPath(row: models.count, section: 0)
+    if let cell = tableView.cellForRow(at: lastIndexPath) as? CountPlusOneCell {
+      cell.configure(for: state)
+    }
+  }
+
+}
